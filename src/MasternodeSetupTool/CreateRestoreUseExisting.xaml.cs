20538b00e45dddc18ec218c756b11829df2985b2
Kevin Loubser <zeptin@gmail.com>
2023-08-10 00:24:35 +0200

    Initial commit with project references changed to Nuget packages

---
+++ src/MasternodeSetupTool/CreateRestoreUseExisting.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+
+namespace MasternodeSetupTool
+{
+    /// <summary>
+    /// Interaction logic for CreateRestoreUseExisting.xaml
+    /// </summary>
+    public partial class CreateRestoreUseExisting : Window
+    {
+        public enum ButtonChoice
+        {
+            CreateWallet,
+            RestoreWallet,
+            UseExistingWallet
+        }
+
+        private ButtonChoice buttonChoice;
+
+        public CreateRestoreUseExisting()
+        {
+            InitializeComponent();
+        }
+
+        public ButtonChoice Choice
+        {
+            get
+            {
+                return this.buttonChoice;
+            }
+        }
+
+        private void CreateWalletButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.buttonChoice = ButtonChoice.CreateWallet;
+        }
+
+        private void RestoreWalletButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.buttonChoice = ButtonChoice.RestoreWallet;
+        }
+
+        private void UseExistingWalletButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.buttonChoice = ButtonChoice.UseExistingWallet;
+        }
+    }
+}
