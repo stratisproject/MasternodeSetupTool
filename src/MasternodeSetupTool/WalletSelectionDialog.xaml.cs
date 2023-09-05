ffd70bf4a6ee28afbdaf66e6a0528a7d099c05a6
Lazar Prijovic <madrazzl3@gmail.com>
2023-09-05 13:37:56 +0100

    Add wallet selection dialog

---
+++ src/MasternodeSetupTool/WalletSelectionDialog.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MasternodeSetupTool
+{
+    /// <summary>
+    /// Interaction logic for WalletSelectionDialog.xaml
+    /// </summary>
+    public partial class WalletSelectionDialog : Window
+    {
+        public WalletSelectionDialog(List<WalletItem> wallets)
+        {
+            InitializeComponent();
+
+            this.ItemsList.ItemsSource = wallets;
+        }
+
+        public string? SelectedWalletName { get; private set; } = null;
+
+        private void SelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedWallet = (WalletItem)this.ItemsList.SelectedItem;
+
+            if (selectedWallet != null)
+            {
+                this.SelectedWalletName = selectedWallet.Name;
+                Close();
+            }
+        }
+
+        public class WalletItem
+        {
+            public string Name { get; set; }
+
+            public WalletItem(string name, long balance)
+            {
+                this.Name = name;
+                this.Balance = balance;
+            }
+
+            public long Balance { get; set; }
+
+            public string BalanceFormatted
+            {
+                get
+                {
+                    return $"{this.Balance}";
+                }
+            }
+        }
+    }
+}
